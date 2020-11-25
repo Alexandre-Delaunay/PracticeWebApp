d9c71b32682d2cb0f32a733322a7a0f70f7781fd
adelaunay@edieyes.net <adelaunay@edieyes.net>
2020-11-25 14:46:28 +0100

    Create search-prescription

---
+++ PracticeWebApp/ClientApp/src/app/search-prescription/search-prescription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-prescription',
+  templateUrl: './search-prescription.component.html',
+  styleUrls: ['./search-prescription.component.css']
+})
+export class SearchPrescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
